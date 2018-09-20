ea246a3b7016f7f513d742aac1b31bbe14bca859
Eric Laitman <ericlaitman@Erics-MacBook-Pro-2.local>
2018-09-19 22:25:21 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(x)
+if x <= 1
+  return false
+else
+divisors = 2
+  while divisors < x
+    if x % divisors === 0
+      return false
+    else
+      divisors+=1
+    end
+  end
+end
+  return true
+end
