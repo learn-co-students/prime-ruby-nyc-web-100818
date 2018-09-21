d54438a82d68d4dbbcbe30e89503fd35d6c15acc
Austin Luft <ausluft@gmail.com>
2018-09-21 19:33:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  else
+    range = Array (2..Math.sqrt(num))
+    range.each { |i| return false if num % i == 0}
+    true
+  end
+  end
