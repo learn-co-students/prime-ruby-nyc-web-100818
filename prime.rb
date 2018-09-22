e88e30a90057abade4fbe251de038343f9aef47f
Chett Tiller <chett.tiller@gmail.com>
2018-09-22 10:52:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(x)
+
+  if (x < 2)
+    return false
+  elsif (x == 2)
+    return true
+  end
+
+  for i in (2...x)
+    if x % i == 0
+      return false
+    end
+  end
+  true
+end
