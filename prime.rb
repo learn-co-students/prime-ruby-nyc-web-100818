5c2a165aa05de8b6b7126c03ac9299ad5629b698
Henry <hanke.liu@gmail.com>
2018-10-01 19:09:35 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,30 @@
-# Add  code here!
+#require 'pry'
+
+def prime?(number)
+
+if number <= 1
+  return false
+
+elsif number == 2
+  return true
+  #binding.pry
+else
+
+  number_array = (2..number-1).to_a
+  counter = 2
+
+  until counter < (number_array.length)
+
+    if number % counter == 0
+      return false
+    end
+    counter += 1
+
+  end
+
+  return true
+
+
+end
+
+end
