d7866e280227a31291fa52eea2f1b265129a2361
Henry <hanke.liu@gmail.com>
2018-10-01 19:21:34 -0400

    pass?

--- prime.rb
+++ prime.rb
@@ -5,24 +5,32 @@ def prime?(number)
 if number <= 1
   return false
 
-elsif number == 2
+elsif number == 2 || number == 3
   return true
   #binding.pry
 else
-
-  number_array = (2..number-1).to_a
   counter = 2
-
-  until counter < (number_array.length)
-
+  for counter in 2..(number - 1) do
     if number % counter == 0
       return false
+      counter += 1
     end
-    counter += 1
-
   end
+return true
 
-  return true
+  # number_array = (2..number-1).to_a
+  # counter = 2
+  #
+  # until counter < (number_array.length)
+  #
+  #   if number % counter == 0
+  #     return false
+  #   end
+  #   counter += 1
+  #
+  # end
+  #
+  # return true
 
 
 end
